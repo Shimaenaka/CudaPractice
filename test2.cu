d9741a00456a707cb548b54fdd837842e5df3ffc
Shimaenaka <shimaenaka@outlook.com>
2024-06-20 23:18:15 +0800

    ...

--- test2.cu
+++ test2.cu
@@ -1,27 +1,28 @@
 #include<stdio.h>
 #define N 1000
 
-__global__ void add(double *a, double *b, double *c) {
+__global__ void add(int *a, int *b, int *c) {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     if(idx < N) c[idx] = a[idx] + b[idx];
 }
 
 int main() {
-    double a[N], b[N], c[N];
-    double *dev_a, *dev_b, *dev_c;
-    cudaMalloc((void**)&dev_a, N * sizeof(double));
-    cudaMalloc((void**)&dev_b, N * sizeof(double));
-    cudaMalloc((void**)&dev_c, N * sizeof(double));
+    int a[N], b[N], c[N];
+    int *dev_a, *dev_b, *dev_c;
+    cudaMalloc((void**)&dev_a, N * sizeof(int));
+    cudaMalloc((void**)&dev_b, N * sizeof(int));
+    cudaMalloc((void**)&dev_c, N * sizeof(int));
     for(int i = 0; i < N; i++) {
         a[i] = i;
         b[i] = i * i;
     }
-    cudaMemcpy(dev_a, a, N * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_b, b, N * sizeof(double), cudaMemcpyHostToDevice);
-    int M = 128;
+    cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_b, b, N * sizeof(int), cudaMemcpyHostToDevice);
+    int M = 256;
     add<<<(N + M - 1) / M, M>>>(a, b, c);
-    cudaMemcpy(c, dev_c, N * sizeof(double), cudaMemcpyDeviceToHost);
-    for(int i = 0; i < N; i++) printf("%lf + %lf = %lf\n", a[i], b[i], c[i]);
+    cudaDeviceSynchronize();
+    cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
+    for(int i = 0; i < N; i++) printf("%d + %d = %d\n", a[i], b[i], c[i]);
     cudaFree(dev_a);
     cudaFree(dev_b);
     cudaFree(dev_c);
