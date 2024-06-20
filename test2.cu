e16d824541f053f9f48c83aacb5881609a5fb672
Shimaenaka <shimaenaka@outlook.com>
2024-06-20 23:22:44 +0800

    .....

--- test2.cu
+++ test2.cu
@@ -1,12 +1,12 @@
 #include<stdio.h>
-#define N 1000
 
-__global__ void add(int *a, int *b, int *c) {
+__global__ void add(int *a, int *b, int *c, int N) {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     if(idx < N) c[idx] = a[idx] + b[idx];
 }
 
 int main() {
+    int N = 100;
     int a[N], b[N], c[N];
     int *dev_a, *dev_b, *dev_c;
     cudaMalloc((void**)&dev_a, N * sizeof(int));
@@ -18,11 +18,11 @@ int main() {
     }
     cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(dev_b, b, N * sizeof(int), cudaMemcpyHostToDevice);
-    int M = 256;
-    add<<<(N + M - 1) / M, M>>>(a, b, c);
-    cudaDeviceSynchronize();
+    add<<<(N + 255) / 256, 256>>>(dev_a, dev_b, dev_c, N);
     cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
-    for(int i = 0; i < N; i++) printf("%d + %d = %d\n", a[i], b[i], c[i]);
+    for(int i = 0; i < N; i++) {
+        printf("%d + %d = %d\n", a[i], b[i], c[i]);
+    }
     cudaFree(dev_a);
     cudaFree(dev_b);
     cudaFree(dev_c);
