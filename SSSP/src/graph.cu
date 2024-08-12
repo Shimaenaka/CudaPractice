90898499d9fe6e4a9493fe54249a7f38acdc6247
Shimaenaka <shimaenaka@outlook.com>
2024-08-12 16:34:20 +0800

    a

--- SSSP/src/graph.cu
+++ SSSP/src/graph.cu
@@ -1,17 +1,17 @@
-#include "graph.h"
-#include <cuda_runtime.h>
-#include <iostream>
+// #include "graph.h"
+// #include <cuda_runtime.h>
+// #include <iostream>
 
-__global__ void relaxEdges(Edge* edges, int* distances, int E, bool* updated) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < E) {
-        int u = edges[i].src;
-        int v = edges[i].dest;
-        int weight = edges[i].weight;
+// __global__ void relaxEdges(Edge* edges, int* distances, int E, bool* updated) {
+//     int i = blockIdx.x * blockDim.x + threadIdx.x;
+//     if (i < E) {
+//         int u = edges[i].src;
+//         int v = edges[i].dest;
+//         int weight = edges[i].weight;
 
-        if (distances[u] != INT_MAX && distances[u] + weight < distances[v]) {
-            distances[v] = distances[u] + weight;
-            *updated = true;
-        }
-    }
-}
+//         if (distances[u] != INT_MAX && distances[u] + weight < distances[v]) {
+//             distances[v] = distances[u] + weight;
+//             *updated = true;
+//         }
+//     }
+// }
--- SSSP/src/sssp.cu
+++ SSSP/src/sssp.cu
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <limits.h>
 #include "graph.h"
-#include "graph.cu"
+
+__global__ void relaxEdges(Edge* edges, int* distances, int E, bool* updated) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < E) {
+        int u = edges[i].src;
+        int v = edges[i].dest;
+        int weight = edges[i].weight;
+
+        if (distances[u] != INT_MAX && distances[u] + weight < distances[v]) {
+            distances[v] = distances[u] + weight;
+            *updated = true;
+        }
+    }
+}
 
 void sssp(Graph& graph, int src);
 
